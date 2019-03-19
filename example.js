7ede87d9d3f8fd0685642968fe4f2d20014f4708
unknown <yvroseaime@gmail.com>
2019-03-19 12:56:05 -0400

    added new files

---
+++ example.js
@@ -0,0 +1,15 @@
+// a function to show date
+function showdate(){
+    document.getElementById("date").innerHTML=Date();
+}// a function to add two numbers
+function add(){
+    var a = 10
+    var b = 20
+    // adds two numbers
+    result = a + b;
+    document.getElementById("result").innerHTML = result;
+}
+//*TOP 3 things you will need to need to write notes about in coding.
+ //1.functionalyt of function, what the will do
+ //2.the parameters the function takes
+ //3.and what output it will give
